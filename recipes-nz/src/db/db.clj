94df0609e1f48d4d78e1158df2fe671d34328707
Milica Karic <karic.milica@gmail.com>
2013-04-05 11:52:41 +0200

    add recipe search by id and ingredient

--- recipes-nz/src/db/db.clj
+++ recipes-nz/src/db/db.clj
@@ -8,3 +8,17 @@
 (mg/connect!)
 (mg/set-db! (mg/get-db "my-app"))
 )
+
+(defn getIngredientCategories [] (mc/find-maps "ingredient"))
+
+(defn addLinkHref [recipe] (let [id (.toString (:_id recipe))]
+                             (assoc recipe :linkHref (str "http://localhost:8080/recipe/" id "/"))))
+
+(defn addLinkHrefToRecipes [inp] (map #(addLinkHref %) inp))
+
+(defn findRecipesByCategory [category] 
+  (let [results (mc/find-maps "recipes" {:ingredient-categories (ObjectId. category)})
+        results-with-href (addLinkHrefToRecipes results)]
+    results-with-href))
+
+(defn findRecipeById [id] (mc/find-map-by-id "recipes" (ObjectId. id)))
