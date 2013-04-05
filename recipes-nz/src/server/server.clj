679e96a01e2e51ab7dd5d82d6f15f809e463019e
Milica Karic <karic.milica@gmail.com>
2013-04-05 11:55:43 +0200

    add login, search and recipe page

--- recipes-nz/src/server/server.clj
+++ recipes-nz/src/server/server.clj
@@ -1,9 +1,24 @@
 (ns server.server
   (:require [db.db :as db]
-            [noir.server :as server]))
+            [noir.server :as server]
+            [views.views :as views]
+            [noir.core :refer [defpage defpartial]]))
 
 (defn start-server [] (server/start 8080)
   (db/db-init))
+
+(defpage "/login" {}
+         (views/login-template "Login"))
+
+(defpage "/search/:category/" {:keys [category]} 
+  (views/search-recipes-template "Recipes"
+                         (db/findRecipesByCategory category)
+                         (db/getIngredientCategories)
+                     ))
+
+(defpage "/recipe/:id/" {:keys [id]} 
+  (views/recipe-template (db/findRecipeById id) (db/getIngredientCategories)
+                     ))
 
 (defn -main [& args]
   (start-server))
